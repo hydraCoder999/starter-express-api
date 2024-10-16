9b48b1089291392c5c9e35309a80bf31170da947
hydraCoder999 <139739919+hydraCoder999@users.noreply.github.com>
2024-10-16 21:50:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Node JS Applications '
+    res.send('Sample Node ')
+    res.send('Node JS Applications ')
+})
 app.listen(process.env.PORT || 3000)
